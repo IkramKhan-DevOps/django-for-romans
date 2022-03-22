69e5b05b2dc312212a7a31aadc5512c399ded079
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-03-22 08:22:29 +0500

    LABS > LAB1: exercise3 added - complete

---
+++ src/labs/lab1/exercise3/Main.java
@@ -0,0 +1,17 @@
+package labs.lab1.exercise3;
+
+public class Main {
+
+    public static void main(String[] args){
+        Circle c4 = new Circle();
+        c4.setRadius(5.0);
+        c4.setColor("Green");
+
+        System.out.println("radius is: " + c4.getRadius());
+        System.out.println("color is: " + c4.getColor());
+
+        // returns description about the instance
+        System.out.println(c4.toString());
+
+    }
+}
